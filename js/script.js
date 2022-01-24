6bc2f68e3ba822fbadf6a7c0a351d959365fe041
Nico Velazquez <nicovege@gmail.com>
2022-01-24 03:49:36 -0300

    pagina error

--- js/script.js
+++ js/script.js
@@ -89,3 +89,8 @@ $(() => {
         })
     })
     // FIN CARRITO
+
+    // PRELOADER
+    window.addEventListener("load", function () {
+        this.document.getElementById("loader").classList.toggle("loader2")
+    })
